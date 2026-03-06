aee60fda42f57854cf11fa008fbf2d01a20e525b
Albasoft-tech <albasoft.tech@gmail.com>
2026-03-06 04:12:48 +0100

    bug

--- routes/generationRoutes.js
+++ routes/generationRoutes.js
@@ -77,7 +77,7 @@ router.post(
 
       const customerId = eventData.data.customerId;
       const customerResponse = await paddle.customers.get(customerId);
-      console.log(customerResponse);
+      const email = customerResponse.email;
 
       if (!email) {
         console.error("No email found for customer:", customerId);
